c7724c372678ddc892ba29024992dd9f0cbc8d8b
radcarver <dualmule92@gmail.com>
2017-05-30 07:16:40 -0400

    Add documentation and more movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,22 +1,46 @@
 import movies
 import fresh_tomatoes
 
-step_brothers = movies.Movie("Step Brothers",
+
+#list of movies & attributes used in website
+#-------
+step_brothers = movies.Movie(
+	"Step Brothers",
 	"Two grown children do a bunch of goofy stuff",
 	"https://upload.wikimedia.org/wikipedia/en/d/d9/StepbrothersMP08.jpg",
 	"https://www.youtube.com/watch?v=CewglxElBK0")
+george_jungle = movies.Movie(
+	"George of the Jungle",
+	"A Man raised in the jungle goes on an adventure",
+	"https://upload.wikimedia.org/wikipedia/en/f/f8/George_Of_The_Jungle.jpg",
+	"https://www.youtube.com/watch?v=_OHAUghSoEA")
+goldeneye = movies.Movie(
+	"Goldeneye",
+	"James Bond takes on an evil villan",
+	"https://upload.wikimedia.org/wikipedia/en/2/24/GoldenEye_-_UK_cinema_poster.jpg",
+	"https://www.youtube.com/watch?v=HHFXthl5IJo")
+gilmore = movies.Movie(
+	"Happy Gilmore",
+	"A crazy amature golfer takes on the pros",
+	"https://upload.wikimedia.org/wikipedia/en/b/be/Happygilmoreposter.jpg",
+	"https://www.youtube.com/watch?v=-6sT7MSJ4GE")
+hot_shots = movies.Movie(
+	"Hot Shots!",
+	"A spoof on Top Gun",
+	"https://upload.wikimedia.org/wikipedia/en/b/b5/Hot_Shots_2.jpg",
+	"https://www.youtube.com/watch?v=WjDhBBUOEdo")
+bourne = movies.Movie(
+	"The Bourne Ultimatum",
+	"A brainwashed assasin discovers his past",
+	"https://upload.wikimedia.org/wikipedia/en/f/fe/The_Bourne_Ultimatum_%282007_film_poster%29.jpg",
+	"https://www.youtube.com/watch?v=N6J2oiKJr-A")
+#--------
 
-#print step_brothers.storyline
 
-#step_brothers.showtrailer()
+movie_list = [step_brothers, george_jungle, goldeneye, gilmore, hot_shots, bourne]
 
+fresh_tomatoes.open_movies_page(movie_list)	
 
-movies2 = []
-for _ in range (0,8):
-	movies2.append(step_brothers)
-
-#fresh_tomatoes.open_movies_page(movies)	
-print(movies.Movie.__module__)
 
 
 
--- movies.py
+++ movies.py
@@ -3,13 +3,25 @@ import webbrowser
 
 
 class Movie():
+	'''
+	Creates a class, Movie, to storing movie information and open movie trailers
+
+	Attributes:
+		title: Title of the movie
+		storyline: One line summary of the movie
+		poster_image_url: URL of the poster from wikipedia
+		trailer_youtube_url: YouTube URL of the trailer
+	'''
+
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+		''' initializes title, storyline, poster_image_url and trailer_youtube_url '''
 		self.title =  movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 
 	def showtrailer(self):
+		''' opens the trailer in the user's browser''' 
 		webbrowser.open(self.trailer_youtube_url)
 
 
